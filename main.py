bebb8fe309d6bfbb77b0bf825f49f2be4f2e6836
teertha <teertha.sarker.3@gmail.com>
2025-04-15 02:44:37 +0600

    Retuen temparature for any station and date

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
+@app.route('/api/v1/<station>/<date>')
+def get_data(station, date):
+    filename = "data-small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date, "   TG"].squeeze()/10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
-@app.route("/Tapi/v1/<station>/<date>")
-def about(station, date):
-    return render_template("about.html")
-
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
